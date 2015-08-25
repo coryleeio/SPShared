c3caa336e818e56c043b47fac0edfe2f9063ab59
Cory Lee <cl10001123@gmail.com>
2015-08-25 00:44:05 -0500

    First ECS classes added to shared, with prerequisites for artemis

---
+++ SPShared/ECS/Components/Velocity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SPShared.ECS.Components
+{
+    class Velocity : Artemis.ComponentPoolable
+    {
+    }
+}
---
+++ SPShared/ECS/Systems/PhysicsSystem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Artemis;
+using Artemis.System;
+using SPShared.ECS.Components;
+
+namespace SPShared
+{
+    class PhysicsSystem : EntityProcessingSystem{
+        public PhysicsSystem() : base(Aspect.All(typeof(Transform), typeof(Velocity))) { }
+
+        public override void Process(Entity entity)
+        {
+
+
+
+
+            throw new NotImplementedException();
+        }
+    }
+}
